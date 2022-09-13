b4cd7c9fd707051f9cbc8550efa8297c57021c62
alexaoh <alexaoh@users.noreply.github.com>
2022-09-13 14:21:27 +0200

    Got results from some experiments (Ex1). Still missing feasibil.
    Collect questions for tomorrow!

--- MCCEalgo1Outline.R
+++ MCCEalgo1Outline.R
@@ -32,8 +32,14 @@ normalize <- function(x) {
   return((x- min(x))/(max(x)-min(x)))
 }
 
-for (c in cont){
-  adult.data[,c] <- normalize(adult.data[,c])
+# Save the minimums and maximums such that I can transform back later!
+mins <- c()
+maxs <- c()
+
+for (j in cont){
+  adult.data[,j] <- normalize(adult.data[,j])
+  mins <- c(mins, min(adult.data[,j]))
+  maxs <- c(maxs, max(adult.data[,j]))
 }
 
 summary(adult.data) # Now the data has been normalized. 
@@ -174,7 +180,7 @@ plot_tree(12)
 
 ############################### Generate counterfactuals based on trees etc. 
 # Generate counterfactual per sample. 
-generate <- function(h, K = K){ # Use K from above as standard.
+generate <- function(h, K = 100){ # Use K from above as standard. K = 10000 is used in the article for the experiments.
   # Instantiate entire D_h-matrix for all features. 
   D_h <- as.data.frame(matrix(data = rep(NA, K*p), nrow = K, ncol = p))
   colnames(D_h) <- c(fixed_features, mut_features)
@@ -182,28 +188,26 @@ generate <- function(h, K = K){ # Use K from above as standard.
 
   # Fill the matrix D_h with copies of the vectors of fixed features. 
   # All rows should have the same value in all the fixed features. 
-  D_h[,fixed_features] <- h %>% dplyr::select(all_of(fixed_features))
+  D_h[, fixed_features] <- h %>% dplyr::select(all_of(fixed_features))
   D_h[, mut_features] <- h %>% dplyr::select(all_of(mut_features)) # Add this to get the correct datatypes (these are not used when predicting though!)
 
   # Now setup of D_h is complete. We move on to the second part, where we append columns to D_h. 
   
   for (j in 1:q){
-    cat("feat",feature_regressed <- mut_features[j], "\n")
-    cat("dtype",feature_regressed_dtype <- mut_datatypes[[j]], "\n")
+    feature_regressed <- mut_features[j]
+    feature_regressed_dtype <- mut_datatypes[[j]]
     
     d <- rep(NA, K) # Empty vector of length K. 
     # Will be inserted into D_h later (could col-bind also, but chose to instantiate entire D_h from the beginning).
     
     for (i in 1:K){
       # Add a single sample from the end node of tree T_j[j] based on data D_h[i,u+j] to d[i].
-      cat("end",end_node_distr <- predict(T_j[[j]], newdata = D_h[i,1:(u+j-1)]), "\n")
+      end_node_distr <- predict(T_j[[j]], newdata = D_h[i,1:(u+j-1)])
       sorted <- sort(end_node_distr, decreasing = T, index.return = T)
       largest_class <- sorted$x
       largest_index <- sorted$ix
-      cat("largest",largest_class,"\n")
-      cat("largest_index",largest_index,"\n")
       if (feature_regressed_dtype == "factor"){
-        cat("s:",s <- runif(1),"\n")
+        s <- runif(1)
         if (s >= largest_class[1]){ # This only works for two classes at this point!
           d[i] <- levels(adult.data[,feature_regressed])[largest_index[2]]
         } else {
@@ -215,7 +219,9 @@ generate <- function(h, K = K){ # Use K from above as standard.
     }
     D_h[,u+j] <- d # Add all the tree samples based on the jth mutable feature to the next column. 
   }
-  D_h %>% mutate_if(is.character,as.factor) # Change characters to factors! THIS IS NOT TESTED THOROUGHLY BUT SEEMS TO WORK OK.
+  D_h[,colnames(adult.data)[-length(colnames(adult.data))]] %>% mutate_if(is.character,as.factor) # Change characters to factors! THIS IS NOT TESTED THOROUGHLY BUT SEEMS TO WORK OK.
+  # We also rearrange the columns to match the column orders in the original data. 
+  # This is an implementation detail that is done to be able to easier calculated sparsity etc in the pre-processing. 
 }
  
 # Points we want to explain. This is the list of factuals. 
@@ -223,74 +229,173 @@ generate <- function(h, K = K){ # Use K from above as standard.
 # this prediction into 1. This is done in the post-processing after generating all the possible counterfactuals. According to the experiments in the article
 # we only generate one counterfactual per factual, for the first 100 undesirable observations we want to explain.
 preds <- prediction_model(test, method = "logreg")
-preds_sorted <- sort(preds, decreasing = F, index.return = T)
-preds_sorted_values <- preds_sorted$x[1:100]
-preds_sorted_indices <- preds_sorted$ix[1:100]
-#preds[preds >= 0.5] <- 1
-#preds[preds < 0.5] <- 0
-new_predicted_data <- cbind(test[preds_sorted_indices,colnames(adult.data)], "y_pred" = preds_sorted_values)
-H <- new_predicted_data[, -which(names(new_predicted_data) %in% "y_pred")] 
+#preds_sorted <- sort(preds, decreasing = F, index.return = T) # Vil tro det kanskje ikke er dette de er på jakt etter!?
+#preds_sorted_values <- preds_sorted$x[1:10] # Skal egentlig ha de 100 første! Gjør dette for testing nå!
+#preds_sorted_indices <- preds_sorted$ix[1:10]
+#new_predicted_data <- cbind(test[preds_sorted_indices,colnames(adult.data)], "y_pred" = preds_sorted_values)
+new_predicted_data <- cbind(test[,colnames(adult.data)], "y_pred" = preds)
+H <- new_predicted_data[new_predicted_data$y_pred < 0.5, ] 
+H <- H[1:20,-which(names(new_predicted_data) %in% c("y_pred","y"))] # Prøver bare med de 10 første foreløpig.
+# We select the 100 test observations with the lowest predicted probability? Perhaps this is not what they mean by 
+# "the first 100 observations with an undesirable prediction"? 
+# I have gone away from this for now!
 
 # Generation of counterfactuals for each point, before post-processing.
-generate_counterfact_for_H <- function(){
+generate_counterfact_for_H <- function(H_l = H){
   D_h_per_point <- list()
-  for (i in 1:nrow(H)){
+  for (i in 1:nrow(H_l)){
     # x_h is a factual. 
-    x_h <- H[i,]
-    D_h_per_point[[i]] <- generate(x_h)
+    x_h <- H_l[i,]
+    D_h_per_point[[i]] <- generate(x_h, K = 500) # I artikkelen hadde de 10000.
+    cat("Generated for point ",i,"\n")
   }
+  D_h_per_point
 }
 
-x_h <- H[1,]
-D_h <- generate(x_h, K = 100)
+D_h_per_point <- generate_counterfact_for_H() # Generate the matrix D_h for each factual we want to explain (in H)
+save(D_h_per_point, file = "H20K500.RData") # Save the generated D_h per point with K = 100 for the first 100 undesirable predictions.
+#load("H10K1000.RData", verbose = T)
 
-#################################### Post-processing.
-# Remove the rows of D_h not satisfying the listed criteria. 
+
+######################################## Post-processing.
+# Remove the rows of D_h (per point) not satisfying the listed criteria. 
+
+fulfill_crit3_D_h <- function(D_h, c, pred.method){
+  D_h_crit3 <- D_h[prediction_model(D_h, method = pred.method) >= c,] # prediction_model(*) is the R function that predicts 
+  # according to the model we want to make explanations for. 
+  # We can see that many rows are the same. The duplicates are removed below. 
+  unique_D_h <- unique(D_h_crit3)
+}
 
 # Fulfill criterion 3.
-c <- 0.5 # Threshold for removal. Not sure what value this should have to begin with. We assume a binary response in our case. 
-D_h_crit3 <- D_h[prediction_model(D_h, method = "logreg") >= c,] # prediction_model(*) is the R function that predicts 
-# according to the model we want to make explanations for. 
-# We can see that many rows are the same. The duplicates are removed below. 
-unique_D_h <- unique(D_h_crit3)
+fulfill_crit3 <- function(D_h_pp, c = 0.5, pred.method = "logreg"){
+  for (i in 1:length(D_h_pp)){
+    D_h <- D_h_pp[[i]]
+    D_h_pp[[i]] <- fulfill_crit3_D_h(D_h, c, pred.method)
+  }
+  D_h_pp
+}
 
-# fulfilling criterion 4.
-# Sparsity and Gower's distance.
-# These are calculated for each x_h separately!
+crit3_D_h_per_point <- fulfill_crit3(D_h_pp = D_h_per_point) # Fullfil criterion 3 for all (unique) generated possible counterfactuals. 
+
+##### fulfilling criterion 4.
+# Calculate Sparsity and Gower's distance for each D_h (per point).
 
 # Calculate Sparsity first: Number of features changed between x_h and the counterfactual.
-unique_D_h$sparsity <- rep(NA, nrow(unique_D_h))
-for (i in 1:nrow(unique_D_h)){
-  unique_D_h[i,"sparsity"] <- sum(x_h != unique_D_h[i,-ncol(unique_D_h)]) 
-}
-
-# Calculate Gower's distance next. 
-library(gower) # Could try to use this package instead of calculating everything by hand below!
-unique_D_h$gower <- rep(NA, nrow(unique_D_h))
-unique_D_h$gowerpack <- rep(NA, nrow(unique_D_h))
-for (i in 1:nrow(unique_D_h)){
-  g <- 0 # Sum for Gower's distance.
-  p <- ncol(x_h)
-  dtypes <- sapply(x_h[colnames(x_h)], class)
-  for (j in 1:p){ # Assuming that the features are already normalized! Perhaps they need to be normalized again!?
-    d_j <- unique_D_h[i,j]
-    if (dtypes[j] == "numeric"){
-      R_j <- 1 # normalization factor, see not in line above.
-      g <- g + 1/R_j*abs(d_j-x_h[,j])
-    } else if (dtypes[j] == "factor"){
-      if (x_h[,j] != d_j){
-        g <- g + 1
-      }
+sparsity_D_h <- function(x_h,D_h){
+  # Calculates sparsity for one counterfactual x_h.
+  D_h$sparsity <- rep(NA, nrow(D_h))
+  if (nrow(D_h) >= 1){
+    for (i in 1:nrow(D_h)){
+      D_h[i,"sparsity"] <- sum(x_h != D_h[i,-ncol(D_h)]) # We remove the last column, which is D_h$sparsity!
     }
   }
-  # Disse to er ulike!! Finn ut hvorfor!?
-  unique_D_h[i,"gower"] <- g/p
-  unique_D_h[i,"gowerpack"] <- gower_dist(x_h,unique_D_h[i,])
+  return(D_h)
 }
 
+sparsity_D_h_all_points <- function(D_h_pp, H_l){
+  # Calculates sparsity for all counterfactuals and adds the column to each respective D_h.
+  for (i in 1:length(D_h_pp)){
+    D_h_pp[[i]] <- sparsity_D_h(H_l[i,], D_h_pp[[i]])
+  }
+  return(D_h_pp)
+}
 
-########################### Performance metrics.
-# Violation: Number of actionability constraints violated by the counterfactual.
+# Testing for sparsity, seems to work fine!
+sparsity_D_h(H[2,],crit3_D_h_per_point[[2]])
+sparsity_D_h(H[1,],crit3_D_h_per_point[[1]])
+sparsity_D_h(H[3,],crit3_D_h_per_point[[3]])
+sparsity_D_h(H[4,],crit3_D_h_per_point[[4]])
+
+spars_D_h_per_point <- sparsity_D_h_all_points(crit3_D_h_per_point,H)
+spars_D_h_per_point[[2]]
+spars_D_h_per_point[[1]]
+spars_D_h_per_point[[3]]
+spars_D_h_per_point[[4]]
+
+gower_D_h <- function(x_h, D_h){
+  # Calculates Gower's distance for one counterfactual x_h.
+  library(gower) # Could try to use this package instead of calculating everything by hand below!
+  D_h$gower <- rep(NA, nrow(D_h))
+  D_h$gowerpack <- rep(NA, nrow(D_h)) # Result from package.
+  dtypes <- sapply(x_h[colnames(x_h)], class)
+  
+  if (nrow(D_h) >= 1){
+    for (i in 1:nrow(D_h)){
+      g <- 0 # Sum for Gower's distance.
+      p <- ncol(x_h)
+      
+      for (j in 1:p){ # Assuming that the features are already normalized! Perhaps they need to be normalized again!?
+        d_j <- D_h[i,j]
+        if (dtypes[j] == "numeric"){
+          R_j <- 1 # normalization factor, see not in line above.
+          g <- g + 1/R_j*abs(d_j-x_h[,j])
+        } else if (dtypes[j] == "factor"){
+          if (x_h[,j] != d_j){
+            g <- g + 1
+          }
+        }
+      }
+      # Disse to er ulike!! Finn ut hvorfor!? Noe med normaliseringen jeg nevner ovenfor å gjøre?
+      # Det kommer en warning om "zero or non-finite range" ved bruke av pakken!
+      # Kanskje jeg bare skal sjekke at det gir mening det jeg har gjort manuelt først!
+      D_h[i,"gower"] <- g/p
+      #D_h[i,"gowerpack"] <- gower_dist(x_h,D_h[i,colnames(x_h)])
+    }
+  }
+  return(D_h)
+}
+
+gower_D_h_all_points <- function(D_h_pp, H_l){
+  # Calculates Gower's distance for all counterfactuals and adds the column to each respective D_h.
+  for (i in 1:length(D_h_pp)){
+    D_h_pp[[i]] <- gower_D_h(H_l[i,], D_h_pp[[i]])
+  }
+  return(D_h_pp)
+}
+
+# Tester Gower's distance om det virker korrekt i implementasjonen. Virker som at denne delen av implementasjonen er god!
+gower_D_h(H[1,],spars_D_h_per_point[[1]])
+gower_D_h(H[2,],spars_D_h_per_point[[2]])
+
+crit4_D_h_all_points <- gower_D_h_all_points(D_h_pp = spars_D_h_per_point, H_l = H)
+crit4_D_h_all_points[[1]]
+crit4_D_h_all_points[[2]]
+all.equal(crit4_D_h_all_points[[2]],gower_D_h(H[2,],spars_D_h_per_point[[2]]))
+
+generate_one_counterfactual_D_h <- function(D_h){
+  # Generate one counterfactual for one factual, i.e. reduce the corresponding D_h to 1 row.
+  if (nrow(D_h) >= 1){
+    # Find minimum sparsity among all rows, remove all rows with sparsity larger than this.
+    min_sparsity <- min(D_h$sparsity)
+    D_h <- D_h[D_h$sparsity == min_sparsity, ]
+    
+    # Next: Among the remaining rows, find the row with the smallest Gower distance.   
+    index <- which(D_h$gower == min(D_h$gower), arr.ind = T)
+    D_h <- D_h[index,]
+  }
+  return(D_h)
+}
+
+generate_one_counterfactual_all_points <- function(D_h_pp){
+  # Generate one counterfactual for all factuals, i.e. reduce each D_h to 1 row. 
+  for (i in 1:length(D_h_pp)){
+    gen <- generate_one_counterfactual_D_h(D_h_pp[[i]])
+    if (nrow(gen) > 1){ # It may happen that a several possible counterfactuals have the same sparsity and Gower distance.
+                        # In such cases we simply choose the first one.
+      D_h_pp[[i]] <- gen[1,]
+    } else {
+      D_h_pp[[i]] <- gen
+    }
+    
+  }
+  return(D_h_pp)
+}
+
+final_counterfactuals <- generate_one_counterfactual_all_points(crit4_D_h_all_points)
+
+########################### Performance metrics. All these should be calculated on "final_counterfactuals"!
+# Violation: Number of actionability constraints violated by the counterfactual. 
 # This should inherently be zero if I have implemented the algorithm correctly!! 
 # Thus, this is an ok check to do. 
 unique_D_h$violation <- rep(NA, nrow(unique_D_h))
@@ -340,4 +445,21 @@ feasibility <- function(){
 
 # Experiment 1:
 # Averages of all the metrics calculated and added to unique_D_h
-# Prøver både med logreg og ANN. 
+# Prøver kun med logreg nå!
+
+L0s <- c()
+L2s <- c()
+N_CEs <- rep(NA, length(final_counterfactuals))
+for (i in 1:length(final_counterfactuals)){
+  l <- final_counterfactuals[[i]]
+  n <- nrow(l)
+  N_CEs[i] <- n
+  if (n >= 0){
+    L0s <- c(L0s,l$sparsity)
+    L2s <- c(L2s,l$gower)
+  } 
+}
+
+
+exp1_MCCE <- data.frame("L0" = mean(L0s), "L2" = mean(L2s), "N_CE" = sum(N_CEs))
+knitr::kable(exp1_MCCE)
