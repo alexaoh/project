03fd2d8b866fd92ed4bcdf67b8db8d3c6ad1f72e
alexaoh <alexaoh@users.noreply.github.com>
2022-09-19 10:31:33 +0200

    Added new results from Markov (second run) and deleted some old.

--- check_results.R
+++ check_results.R
@@ -1,18 +1,21 @@
 # Open the data resulting from the simulations done on Markov.
 
-load("../results/D_hs/logreg_H100_K10000_binT.RData", verbose = T)
+setwd("/home/ajo/gitRepos/project")
+
+load("results/D_hs/logreg_H100_K10000_binT.RData", verbose = T)
 
 length(D_h_per_point)
 dim(D_h_per_point[[1]])
 
 
 rm(list = ls())
-load("../results/final_counterfactuals_logreg_H100_K10000_binT.RData", verbose = T)
-length(D_h_per_point)
-dim(D_h_per_point[[5]])
-
+load("results/final_counterfactuals_logreg_H100_K10000_binT.RData", verbose = T)
+length(final_counterfactuals)
+dim(final_counterfactuals[[5]])
+final_counterfactuals[[1]]
 
 rm(list = ls())
-load("../results/final_counterfactuals.RData", verbose = T)
-length(D_h_per_point)
-dim(D_h_per_point[[5]])
+load("results/final_counterfactuals_ANN_H100_K10000_binT.RData", verbose = T)
+length(final_counterfactuals)
+dim(final_counterfactuals[[5]])
+final_counterfactuals[[1]]
